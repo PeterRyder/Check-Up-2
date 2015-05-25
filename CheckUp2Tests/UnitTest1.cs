6aaf8e102247a4b7c15f52fe7b4d628fc7499a48
PeterRyder <peter.w.ryder@gmail.com>
2015-05-25 02:38:30 -0400

    Initial commit of test window, unit tests project and appveyor integration

---
+++ CheckUp2Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CheckUp2Tests {
+    [TestFixture]
+    public class UnitTest1 {
+        [Test]
+        public void TestMethodExample() {
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Check-Up-2
+Check Up 2
+========
+[![Build status](https://ci.appveyor.com/api/projects/status/di5gdkwxhvhvub8q?svg=true)](https://ci.appveyor.com/project/PeterRyder/check-up-2)
